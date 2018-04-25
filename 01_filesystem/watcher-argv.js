5992cdb10b39d80735aaf2b12f817133a1e0a401
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-04-25 21:00:14 +0200

    watching for file change and piping child process stdout to process stdout

---
+++ 01_filesystem/watcher-argv.js
@@ -0,0 +1,12 @@
+'use strict';
+const fs = require('fs');
+// console.log(process.argv);
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('Please specify a file to watch!');
+}
+
+fs.watch(filename, () => console.log(`File ${filename} changed!`));
+
+console.log(`Now watching ${filename} for changes...`);
---
+++ 01_filesystem/watcher-spawn.js
@@ -0,0 +1,26 @@
+'use strict';
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+// console.log(process.argv);
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('Please specify a file to watch!');
+}
+
+fs.watch(filename, () => {
+    console.log(`File ${filename} changed!`);
+    const ls = spawn('ls', ['-l', '-h', filename]);
+    // ls.stdout.pipe(process.stdout);
+    
+    let output = '';
+    ls.stdout.on('data', chunk => output += chunk);
+
+    ls.on('close', () => {
+        const parts = output.split(/\s+/);
+        console.log(parts[0], parts[4], parts[8]);
+    })
+
+});
+
+console.log(`Now watching ${filename} for changes...`);
