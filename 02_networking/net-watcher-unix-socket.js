c479e93c949a36f55984dc10905316510ab5bb64
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-04-27 20:15:13 +0200

    create tcp and socket server

---
+++ 02_networking/net-watcher-unix-socket.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const net = require('net');
+const port = 60300;
+const filename = process.argv[2];
+
+if (!filename) {
+    throw Error('Error: No file specified.');
+}
+
+net.createServer(connection => {
+    // Reporting
+    console.log('Subscriber connected.');
+    connection.write(`Now watching "${filename}" for changes...\n`);
+
+    // Watcher setup
+    const watcher = fs.watch(filename, () => connection.write(`File "${filename}" changed: ${new Date()} \n`));
+
+    // Clean up
+    connection.on('close', () => {
+        console.log('Subscriber disconnected.');
+        watcher.close();
+    });
+}).listen('/tmp/watcher.sock', () => console.log(`Listening for subscribers on port ${port}...\n`));
