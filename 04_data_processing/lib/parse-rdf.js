183edbf299abe85f81ccfbce281d41144ee44ef9
Jeandre Rohlandt <jrohlandt@gmail.com>
2018-05-04 21:34:35 +0200

    installed cheerio and extract book id from rdf document

--- 04_data_processing/lib/parse-rdf.js
+++ 04_data_processing/lib/parse-rdf.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const cheerio = require('cheerio');
+
 module.exports = rdf => {
 
+    const $ = cheerio.load(rdf);
+
+    const book = {};
+
+    book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
+    
+    return book;
 };
--- 04_data_processing/test/parse-rdf-test.js
+++ 04_data_processing/test/parse-rdf-test.js
@@ -6,11 +6,16 @@ const assert = require('assert');
 const parseRDF = require('../lib/parse-rdf.js');
 
 console.log('dir: ', __dirname);
-const rdf = fs.readFileSync(`${__dirname}/../../data/cache/epub/132/pg132.rdf`);
+const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
 
 describe('parseRDF', () => {
     it('should be a function', () => {
         assert.ok(typeof parseRDF === 'function', 'parseRDF should be a function.');
         // expect(parseRDF).to.be.a('function');
+    });
+    
+    it('should parse RDF content', () => {
+        const book = parseRDF(rdf);
+        expect(book).to.be.an('object');
     });
 });
